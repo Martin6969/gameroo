92123ef4e79af320f92e8bdadff87d2527d7a740
Martin6969 <martintonjac13@gmail.com>
2022-10-16 17:01:18 +0200

    V-petlja odrađena
    treća vježba odrađena

--- src/components/GamerooCard.vue
+++ src/components/GamerooCard.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="card text-center">
-  <div class="card-header">
-    Kena
+  <div class="card-header text-left">
+    {{ info.title }}
   </div>
   <div class="card-body p-0">
-    <img class="card-img-top" src="@/assets/kena.webp"/>
+    <img class="card-img-top" :src="info.url "/>
   </div>
   <div class="card-footer text-muted">
-    2 days ago
+    {{ info.time }}
   </div>
 </div>
 </template>
 
 <script>
 export default {
+  props:["info"],
   name: 'GamerooCard',
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <template>
  <div class="row">
  <div class="col-6">ovo je cirkus
-  <gameroo-card />
-  <gameroo-card-2/>
-  <gameroo-card-3/>
-  <gameroo-card-4/>
+  <gameroo-card v-for="card in cards" :key="card" :info="card" />
+ 
 </div>
  
  <div class="col-5">
@@ -16,18 +14,27 @@
 <script>
 // @ is an alias to /src
 import GamerooCard from '@/components/GamerooCard.vue'
-import GamerooCard2 from '@/components/GamerooCard2.vue'
-import GamerooCard3 from '@/components/GamerooCard3.vue'
-import GamerooCard4 from '@/components/GamerooCard4.vue'
+
+let cards = [];
+
+cards = [
+  { title:"Kena", time:"3 hours ago", url: "https://image.api.playstation.com/vulcan/ap/rnd/202102/2307/kQzDCY5RCrSXCeeFjPGUzkGI.png"},
+ { title:"Dark Souls 3", time:"5 hours ago", url: "https://image.api.playstation.com/cdn/EP0700/CUSA03365_00/OFMeAw2KhrdaEZAjW1f3tCIXbogkLpTC.png"},
+ { title:"AC Odisay", time:"10 hours ago", url:"https://cdn1.epicgames.com/undefined/offer/S2_ACOD-1280x1440-621a727d381ffe0cffe869c1e23bc741.jpg"},
+ { title:"AC Valhalla", time:"12 hours ago", url:"https://cdn.wccftech.com/wp-content/uploads/2020/06/Assassins-Creed-Valhalla-Game-Bundle-Promo.jpg"},
+ ];
+
 export default {
   name: 'homeview',
-  components: {
-    GamerooCard,
-    GamerooCard2,
-    GamerooCard3,
-    GamerooCard4
-  }
-}
+  data: function() {
+    return {
+      cards:cards,
+  };
+},
+  components:{
+  GamerooCard,
+  },
+};
 </script>
 
 <style scoped>
